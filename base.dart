c8df87681cd698ef4cd191280ebc0225ceb8f005
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2022-02-17 16:45:35 -0300

    Update base.dart

--- base.dart
+++ base.dart
@@ -11,44 +11,73 @@ abstract class Cell<T> {
 }
 
 abstract class CellularAutomata<T, C extends Cell<T>> {
-  final Set<int> _shape;
+  final List<int> _shape;
+  int num_cells = 0;
+  List<int> _shape_dimensions = [];
   
   List<C> _grid = [];
   
-  Set<int> get shape => _shape;
+  List<int> get shape => _shape;
   
-  CellularAutomata(this._shape);
+  CellularAutomata(this._shape){
+    num_cells = _shape.reduce((i,j) => i * j);
+    var tmp1 = 1;
+    for(int s in _shape.reversed){
+      tmp1 *= s;
+      _shape_dimensions.insert(0, tmp1);
+    }
+  }
   
   List<C> get grid => _grid;
   
   void initialize() {
-    int size = _shape.reduce((i,j) => i * j);
     _grid = <C>[];
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < num_cells; i++){
       _grid.add(new C());
     }
   }
   
-  int _to_grid_index(int list_index){
+  List<int> _to_grid_index(int list_index){
+    var index = <int>[]; 
     int tmp = list_index;
-    for(int i = 0; i < _shape.length; i++){
-      tmp = tmp % _shape[i];
+    for(int i = 0; i < _shape.length-1; i++){
+      index.add(tmp ~/ _shape_dimensions[i]);
+      tmp = tmp % _shape_dimensions[i];
     }
+    index.add(tmp);
+    return index;
+  }
+  
+  int _to_list_index(List<int> grid_index){
+    int index = 0; 
+    for(int i = 0; i < _shape.length-1; i++){
+      index += grid_index[i] * _shape_dimensions[i];
+    }
+    index += grid_index[_shape.length-1];
+    return index;
   }
   
   List<C> neighborhood(int list_index);
   
   List<T> next(){
-    for(var cell in _grid){
-      
+    List<T> tmp = [];
+    for(int i = 0; i < num_cells; i++){
+      var cell = _grid[i];
+      cell.update(neighborhood(i));
+      tmp.add(cell.state);
     }
+    return tmp;
   }
   
   List<T> run(int iterations){
+    
     for(int i = 0; i < iterations; i++){
-      next();
+      for(int i = 0; i < num_cells; i++){
+        var cell = _grid[i];
+        cell.update(neighborhood(i));
+      }
     }
-    return _grid;
+    return next();
   }
 }
 
