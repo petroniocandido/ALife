1e76f7b5be912c0b66114e169481a976f0786d35
Petrônio Cândido <petronio.candido@gmail.com>
2022-03-14 23:44:10 -0300

    Boids

--- Boids/Boids.dart
+++ Boids/Boids.dart
@@ -1,37 +1,144 @@
 import '../Common/Point2D.dart';
+import 'dart:math';
+
+final _random = Random();
 
 class Boid {
   Point2D _position = Point2D(0, 0);
 
   Point2D get position => _position;
 
-  Point2D _direction = Point2D(0, 0);
+  double _direction = 0;
 
-  Point2D get direction => _direction;
+  double get direction => _direction;
+
+  void set direction(double d) => _direction = d;
+
+  int _id = 0;
+
+  int get id => _id;
+
+  Boid(this._id, this._position, this._direction);
 }
 
-typedef Boid ActionFunction(Boid obj, BoidSimulation sun);
+typedef Boid BoidAction(Boid obj, BoidSimulation sun);
 
-abstract class BoidSimulation<T extends Boid> {
-  var _boids = <T>[];
+class BoidSimulation {
+  List<int> _shape_dimensions = [];
 
-  List<T> get boids => _boids;
+  List<int> get shape_dimensions => _shape_dimensions;
+
+  int _velocity = 1;
+
+  int get velocity => _velocity;
+
+  void set velocity(int v) => _velocity = v;
+
+  double _radius = 2.0;
+
+  double get radius => _radius;
+
+  void set radius(double r) => _radius = r;
+
+  Point2D _swarmMidPoint = Point2D(0, 0);
+
+  Point2D get swarmMidPoint => _swarmMidPoint;
+
+  var _boids = <Boid>[];
+
+  List<Boid> get boids => _boids;
 
   int _numBoids = 0;
 
   int get numBoids => _numBoids;
 
-  var _actions = <ActionFunction>[];
+  var _actions = <BoidAction>[];
+
+  Map<int, Map<int, double>> _distances = {};
 
   // Abstract Methods
 
-  void initialize();
+  void initialize() {
+    for (int id = 0; id < numBoids; id++) {
+      boids.add(Boid(
+          id,
+          Point2D(_random.nextInt(shape_dimensions[0]),
+              _random.nextInt(shape_dimensions[1])),
+          (4 * pi * _random.nextDouble()) - 2 * pi));
+    }
+
+    _actions.add(coherence);
+    _actions.add(separation);
+    _actions.add(move);
+  }
+
+  Boid coherence(Boid obj, BoidSimulation sim) {
+    double a = obj.position.angle(sim.swarmMidPoint);
+    double da = obj.direction - a;
+    obj.direction += da;
+    return obj;
+  }
+
+  List<Boid> neighbors(Boid boid) {
+    List<Boid> ix = [];
+    for (int j = 0; j < boids.length; j++) {
+      if (j != boid.id && (_distances[boid.id]?[j] ?? 0) <= radius) {
+        ix.add(boids[j]);
+      }
+    }
+    return ix;
+  }
+
+  Boid separation(Boid obj, BoidSimulation sim) {
+    List<Boid> neigh = sim.neighbors(obj);
+    Point2D mid = sim.midPoint(neigh);
+    double a = obj.position.angle(mid);
+    double da = obj.direction - a;
+    obj.direction += -da;
+    return obj;
+  }
+
+  Boid move(Boid obj, BoidSimulation sim) {
+    List<Boid> neigh = sim.neighbors(obj);
+    Point2D mid = sim.midPoint(neigh);
+    double a = obj.position.angle(mid);
+    double da = obj.direction - a;
+    obj.direction += -da;
+    return obj;
+  }
 
   // Concrete Methods
 
+  void mapDistances() {
+    for (int i = 0; i < boids.length; i++) {
+      var boidA = boids[i];
+      _distances[boids[i].id] = {};
+      _distances[i]?[i] = -1;
+      for (int j = i + 1; j < boids.length; j++) {
+        var boidB = boids[j];
+        double d = boidA.position.distance(boidB.position);
+        _distances[i]?[j] = d;
+        _distances[j]?[i] = d;
+      }
+    }
+  }
+
+  Point2D midPoint(List<Boid> swarm) {
+    int px = 0;
+    int py = 0;
+    for (Boid boid in swarm) {
+      px += boid.position.x;
+      py += boid.position.y;
+    }
+    px = (px / swarm.length) as int;
+    py = (py / swarm.length) as int;
+
+    return Point2D(px, py);
+  }
+
   void next() {
     for (Boid boid in _boids) {
-      for (ActionFunction func in _actions) {
+      for (BoidAction func in _actions) {
         func.call(boid, this);
       }
     }
--- Common/Point2D.dart
+++ Common/Point2D.dart
@@ -13,7 +13,10 @@ class Point2D {
 
   Point2D(this._x, this._y);
 
-  double distance(Point2D obj) => sqrt((x * obj.x) + (y * obj.y));
+  double distance(Point2D obj) => sqrt((x - obj.x) ^ 2 + (y - obj.y) ^ 2);
+
+  double angle(Point2D obj) =>
+      atan(this.dot(obj) / (this.internal_product() + obj.internal_product()));
 
   Point2D operator +(Point2D obj) {
     return Point2D(this.x + obj.x, this.y + obj.y);
@@ -26,4 +29,17 @@ class Point2D {
   Point2D operator *(num obj) {
     return Point2D((this.x * obj as int), (this.y * obj as int));
   }
+
+  int dot(Point2D obj) => x * obj.x + y * obj.y;
+
+  int internal_product() => this.dot(this);
+}
+
+double fixAngle(double a) {
+  if (-2 * pi > a) {
+    return a + 4 * pi;
+  } else if (a > 2 * pi) {
+    return a - 4 * pi;
+  }
+  return a;
 }
